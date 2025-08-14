ec69da10d8c209c49742f978f742116cf75835b9
Shishir <shishirkhondoker34@gmail.com>
2025-08-14 20:41:55 +0600

    second update first express js code

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ let app = express();
 app.get('/', (req, res)=>{
     res.send('Hello World!');
 })
+app.get ('/about',(req,res)=>{
+    res.send('About page');
+})
 
+app.get('/contact',(req,res)=>{
+    res.send('contact page');
+})
 
 app.listen(5050, ()=>{
     console.log('Server is running on port 5050');
